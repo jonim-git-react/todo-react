4f0467acb97e6a0cc4794c871f2ed9b4f5d161e6
jonim-git <n0majo01@students.oamk.fi>
2021-10-19 19:45:17 +0300

    valmis todo

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ const URL = 'http://localhost/todo/';
 function App() {
   const [tasks, setTasks] = useState([]);
   const [task, setTask] = useState('');
+  const [editTask, setEditTask] = useState(null);
+  const [editDescription, setEditDescription] = useState('');
 
   useEffect(() => {
     axios.get(URL + 'index.php')
@@ -21,17 +23,54 @@ function App() {
 
   function save(e) {
     e.preventDefault();
-    const json = JSON.stringify({description:task})
-    axios.post(URL + 'save.php',json,{
+    const json = JSON.stringify({ description: task })
+    axios.post(URL + 'save.php', json, {
       headers: {
-        'Content-Type' : 'applicationJ/json'
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        setTasks(tasks => [...tasks, response.data]);
+        setTask('');
+      }).catch(error => {
+        alert(error.response.data.error)
+      })
+  }
+
+  function remove(id) {
+    const json = JSON.stringify({ id: id })
+    axios.post(URL + 'delete.php', json, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        const newListWithoutRemoved = tasks.filter((item) => item.id !== id);
+        setTasks(newListWithoutRemoved);
+      }).catch(error => {
+        alert(error.response.data.error)
+      })
+  }
+
+  function setEditedTask(task) {
+    setEditTask(task);
+    setEditDescription(task?.description);
+  }
+
+  function update(e) {
+    e.preventDefault();
+    const json = JSON.stringify({id:editTask.id,description:editDescription})
+    axios.post(URL + 'update.php',json, {
+      headers: {
+        'Content-Type' : 'application/json'
       }
     })
     .then((response) => {
-      setTasks(tasks => [...tasks,response.data]);
-      setTask('');
+      tasks[(tasks.findIndex(task => task.id === editTask.id))].description = editDescription;
+      setTasks([...tasks]);
+      setEditedTask(null);
     }).catch (error => {
-      alert(error.response.data.error)
+      alert(error.response ? error.response.data.error : error);
     })
   }
 
@@ -45,7 +84,26 @@ function App() {
       </form>
       <ol>
         {tasks?.map(task => (
-          <li key={task.id}>{task.description}</li>
+          <li key={task.id}>
+            {editTask?.id !== task.id &&
+              task.description
+            }
+            {editTask?.id === task.id &&
+              <form onSubmit={update}>
+                <input value={editDescription} onChange={e => setEditDescription(e.target.value)} />
+                <button>Save</button>
+                <button type="button" onClick={() => setEditedTask(null)}>Cancel</button>
+              </form>
+            }
+            <a href="#" className="delete" onClick={() => remove(task.id)}>
+               Delete
+            </a>&nbsp;
+            {editTask === null &&
+              <a className="edit" onClick={() => setEditedTask(task)} href="#">
+                 Edit
+              </a>
+            }
+          </li>
         ))}
       </ol>
     </div>
